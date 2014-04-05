3e6a0555afc1a2b9164b52eef08e9fc279850ebc
marianormuro <mariano.muro@gmail.com>
2014-04-05 16:34:11 -0400

    * Added utility to translate OWL ontologies with TBox axioms to OWL ontologies with SWRL rules.

---
+++ obdalib-owlapi3/src/main/java/it/unibz/krdb/obda/owlapi3/swrl/OWLToSWRLOntologyTranslator.java
@@ -0,0 +1,71 @@
+/*
+ * Copyright (C) 2009-2013, Free University of Bozen Bolzano This source code is
+ * available under the terms of the Affero General Public License v3.
+ * 
+ * Please see LICENSE.txt for full license terms, including the availability of
+ * proprietary exceptions.
+ */
+package it.unibz.krdb.obda.owlapi3.swrl;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.Set;
+
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.IRI;
+import org.semanticweb.owlapi.model.OWLAxiom;
+import org.semanticweb.owlapi.model.OWLDataFactory;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.model.SWRLRule;
+
+/**
+ * 
+ * @author Mariano Rodriguez Muro <mariano.muro@gmail.com>
+ * 
+ */
+public class OWLToSWRLOntologyTranslator {
+
+	/**
+	 * 
+	 */
+	public OWLToSWRLOntologyTranslator(OWLDataFactory fac) {
+
+	}
+
+	public static void main(String[] args) {
+		File inputFile = new File(args[0]);
+		File outputFile = new File(args[1]);
+
+		try {
+
+			// Get hold of an ontology manager
+			OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+			// Let's load an ontology from the web
+
+			OWLOntology inputOntology = manager.loadOntologyFromOntologyDocument(inputFile);
+
+			System.out.println("Loaded ontology: " + inputOntology);
+
+			OWLDataFactory owlDataFactory = manager.getOWLDataFactory();
+
+			OWLToSWRLTranslator tr = new OWLToSWRLTranslator(owlDataFactory);
+
+			Set<SWRLRule> rules = tr.translate(inputOntology);
+
+			Set<OWLAxiom> axioms = new HashSet<OWLAxiom>();
+			axioms.addAll(rules);
+
+			OWLOntology result = manager.createOntology(axioms);
+
+			manager.saveOntology(result, IRI.create(outputFile.toURI()));
+
+		} catch (Exception e) {
+
+			e.printStackTrace();
+
+		}
+
+	}
+
+}

6f17c31a3d1de155e57518d2c936620a8ae81667
Petko Rutesic <petko_rutesic@yahoo.com>
2016-05-03 10:11:56 +0200

    Test for new bnodes in grammar- beginning

---
+++ quest-test/src/test/java/it/unibz/krdb/obda/parser/BnodeMappingTest.java
@@ -0,0 +1,215 @@
+package it.unibz.krdb.obda.parser;
+
+/*
+ * #%L
+ * ontop-test
+ * %%
+ * Copyright (C) 2009 - 2014 Free University of Bozen-Bolzano
+ * %%
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * #L%
+ */
+
+import it.unibz.krdb.obda.io.ModelIOManager;
+import it.unibz.krdb.obda.model.OBDADataFactory;
+import it.unibz.krdb.obda.model.OBDAModel;
+import it.unibz.krdb.obda.model.impl.OBDADataFactoryImpl;
+import it.unibz.krdb.obda.ontology.*;
+import it.unibz.krdb.obda.owlapi3.OWLAPI3TranslatorUtility;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestConstants;
+import it.unibz.krdb.obda.owlrefplatform.core.QuestPreferences;
+import it.unibz.krdb.obda.owlrefplatform.core.dagjgrapht.Equivalences;
+import it.unibz.krdb.obda.owlrefplatform.core.dagjgrapht.TBoxReasoner;
+import it.unibz.krdb.obda.owlrefplatform.core.dagjgrapht.TBoxReasonerImpl;
+import it.unibz.krdb.obda.owlrefplatform.owlapi3.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.semanticweb.owlapi.apibinding.OWLManager;
+import org.semanticweb.owlapi.model.OWLIndividual;
+import org.semanticweb.owlapi.model.OWLOntology;
+import org.semanticweb.owlapi.model.OWLOntologyManager;
+import org.semanticweb.owlapi.reasoner.SimpleConfiguration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+/***
+ * Test returns executes queries over the database with mapping
+ * in which bnodes have been used.
+ */
+
+public class BnodeMappingTest {
+
+	private OBDADataFactory fac;
+	private QuestOWLConnection conn;
+	private Connection connection;
+
+	Logger log = LoggerFactory.getLogger(this.getClass());
+	private OBDAModel obdaModel;
+	private OWLOntology ontology;
+
+	final String owlfile = "src/test/resources/emptiesDatabase.owl";
+	final String obdafile = "src/test/resources/emptiesDatabaseWithBnodes.obda";
+	
+//	 final String owlfile =
+//	 "src/main/resources/testcases-scenarios/virtual-mode/stockexchange/simplecq/stockexchange.owl";
+//	 final String obdafile =
+//	 "src/main/resources/testcases-scenarios/virtual-mode/stockexchange/simplecq/stockexchange-mysql.obda";
+//
+	private List<String> emptyConcepts = new ArrayList<String>();
+	private List<String> emptyRoles = new ArrayList<String>();
+	private Set<ClassExpression> emptyBasicConcepts = new HashSet<ClassExpression>();
+	private Set<Description> emptyProperties = new HashSet<Description>();
+
+	private QuestOWL reasoner;
+	private Ontology onto;
+
+	@Before
+	public void setUp() throws Exception {
+
+		String driver = "org.h2.Driver";
+		String url = "jdbc:h2:mem:questjunitdb;";
+		String username = "sa";
+		String password = "";
+
+		fac = OBDADataFactoryImpl.getInstance();
+
+		connection = DriverManager.getConnection(url, username, password);
+		Statement st = connection.createStatement();
+
+		FileReader reader = new FileReader("src/test/resources/emptiesDatabase-h2.sql");
+		BufferedReader in = new BufferedReader(reader);
+		StringBuilder bf = new StringBuilder();
+		String line = in.readLine();
+		while (line != null) {
+			bf.append(line);
+			line = in.readLine();
+		}
+		in.close();
+
+		st.executeUpdate(bf.toString());
+		connection.commit();
+
+		// Loading the OWL file
+		OWLOntologyManager manager = OWLManager.createOWLOntologyManager();
+		ontology = manager.loadOntologyFromOntologyDocument((new File(owlfile)));
+
+		// Loading the OBDA data
+		fac = OBDADataFactoryImpl.getInstance();
+		obdaModel = fac.getOBDAModel();
+
+		ModelIOManager ioManager = new ModelIOManager(obdaModel);
+		ioManager.load(obdafile);
+
+		QuestPreferences p = new QuestPreferences();
+		p.setCurrentValueOf(QuestPreferences.ABOX_MODE, QuestConstants.VIRTUAL);
+		p.setCurrentValueOf(QuestPreferences.OBTAIN_FULL_METADATA, QuestConstants.FALSE);
+		// Creating a new instance of the reasoner
+		QuestOWLFactory factory = new QuestOWLFactory();
+		factory.setOBDAController(obdaModel);
+
+		factory.setPreferenceHolder(p);
+
+		reasoner = (QuestOWL) factory.createReasoner(ontology, new SimpleConfiguration());
+
+		// Now we are ready for querying
+		conn = reasoner.getConnection();
+
+		//onto = OWLAPI3TranslatorUtility.translate(ontology);
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		
+			dropTables();
+			reasoner.dispose();
+			connection.close();
+		
+
+	}
+
+	private void dropTables() throws SQLException, IOException {
+
+		Statement st = connection.createStatement();
+
+		FileReader reader = new FileReader("src/test/resources/emptiesDatabase-drop-h2.sql");
+		BufferedReader in = new BufferedReader(reader);
+		StringBuilder bf = new StringBuilder();
+		String line = in.readLine();
+		while (line != null) {
+			bf.append(line);
+			line = in.readLine();
+		}
+
+		st.executeUpdate(bf.toString());
+		st.close();
+		connection.commit();
+	}
+
+	private int runTests(String query) throws Exception {
+		QuestOWLStatement st = conn.createStatement();
+
+		int results=0;
+		try {
+			QuestOWLResultSet rs = st.executeTuple(query);
+
+			while (rs.nextRow()){
+				OWLIndividual ind1 =	rs.getOWLIndividual("x")	 ;
+				log.debug(ind1.toString());
+				results++;
+			}
+
+		} catch (Exception e) {
+			throw e;
+		} finally {
+			try {
+
+			} catch (Exception e) {
+				st.close();
+			}
+			conn.close();
+			reasoner.dispose();
+		}
+		return results;
+	}
+
+
+	/**
+	 * Test numbers of empty concepts
+	 * 
+	 * @throws Exception
+	 */
+	@Test
+	public void testBNodesMapping() throws Exception {
+		String query = "PREFIX : <http://www.semanticweb.org/smallDatabase#> SELECT ?x WHERE {?x a :Fairtrade}";
+		int numberResults = runTests(query);
+		assertEquals(1, numberResults);
+	}
+
+
+}

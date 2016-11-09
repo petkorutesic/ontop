50504a4bb618f5176abd09beede920f68df973d9
Petko Rutesic <petko_rutesic@yahoo.com>
2016-11-09 15:39:57 +0100

    small change

--- quest-test/src/test/resources/db-noprimarykeys-create-h2.sql
+++ quest-test/src/test/resources/db-noprimarykeys-create-h2.sql
@@ -1,12 +1,13 @@
-
-CREATE TABLE table1 (
-    attr1 int NOT NULL,
-    attr2 int NOT NULL
+CREATE TABLE t_message(
+    message_time int NOT VARCHAR
+    message_text int NOT VARCHAR
 );
 
 
-INSERT INTO table1 VALUES ('1', '1');
-
+INSERT INTO t_message VALUES ('10:00', 'hello');
+INSERT INTO t_message VALUES ('10:00', 'hello');
+INSERT INTO t_message VALUES ('10:00', 'hello');
+INSERT INTO t_message VALUES ('10:00', 'ciao');
 
 
 
